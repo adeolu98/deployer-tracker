5eda5db1492629c521803feeaf5143aa344e950c
dCRYPT <39372980+adeolu98@users.noreply.github.com>
2023-04-14 12:25:42 +0100

    add deployer tracker

---
+++ src/index.test.ts
@@ -0,0 +1,17 @@
+import { contractCreationHexIdentifier } from ".";
+
+const app = require("./index");
+const request = require("supertest");
+
+const deployerAddress = "0x80805ae3cbE23715C1f1807A03C5fb669541C2A9";
+const network = "sepolia";
+
+
+describe("fetch data for a given deployer address on sepolia", () => {
+  it("get contract data", async () => {
+    const res = await request(app).get(`/${deployerAddress}/${network}`);
+    
+    expect(res.status).toBe(200)
+    expect(res.text.includes(contractCreationHexIdentifier)).toBeDefined;
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+var express = require("express");
+var app = express();
+
+const { ethers } = require("ethers");
+export const contractCreationHexIdentifier = "0x608060";
+
+export async function search(deployerAddress, network ) {
+  const etherscanProvider = new ethers.providers.EtherscanProvider(network);
+  const history = await etherscanProvider.getHistory(deployerAddress);
+
+  // search through past tx data for contractCreationHexIdentifier in data field.
+  const filtered = history.filter((tx) =>
+    tx.data.includes(contractCreationHexIdentifier)
+  );
+  return filtered;
+}
+
+// get data and filter it, return only txs with 0x608060 in input data
+app.get(`/:deployerAddr/:network`, async function (req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+
+  const txdata = await search(req.params.deployerAddr, req.params.network);
+
+  res.send({
+    contractsCreated: txdata.map((data) => {
+      return data.creates;
+    }),
+    inputData: txdata.map((data) => {
+      return data.data;
+    }),
+    timestamp: txdata.map((data) => {
+      return data.timestamp;
+    }),
+  });
+});
+
+module.exports = app;
+//app.listen(9000, () => console.log("App listening on port 9000!"));
