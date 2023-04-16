7e79cad6de72c0bf68b395a07f011a0e9f8dc674
dCRYPT <39372980+adeolu98@users.noreply.github.com>
2023-04-16 11:25:58 +0100

    add logic for checking deploys, update to sheety

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ var express = require("express");
 var app = express();
 
 const { ethers } = require("ethers");
-const contractCreationHexIdentifier = "0x608060";
-//const { contractsData } = require("./contracts.ts");
 const fetch = require("node-fetch");
 
+const contractCreationHexIdentifier = "0x608060";
 const networkNames = {
   homestead: 1,
   goerli: 5,
@@ -36,104 +35,90 @@ async function search(deployerAddress, network) {
   const etherscanProvider = new ethers.providers.EtherscanProvider(network);
   const history = await etherscanProvider.getHistory(deployerAddress);
 
-  // search through past tx data for contractCreationHexIdentifier in data field.
-
+  // search through past tx data for contractCreationHexIdentifier in data field and if the deploy happened in last 12 hrs
   const filtered = history.filter((tx) => {
-    tx.data.includes(contractCreationHexIdentifier); // &&(Date.now()/1000) - tx.timestamp <= 43200; // check if the deploy happened in last 12 hrs
+    if (
+      tx.data.includes(contractCreationHexIdentifier) &&
+      Date.now() / 1000 - tx.timestamp <= 43200
+    )
+      return tx;
   });
-  console.log("filtered", filtered);
-
   return filtered;
 }
 
-async function callSheety() {
+async function callSheety(data) {
   let url =
     "https://api.sheety.co/555630a0193000542b1d8d8703523324/liveTimeNewContractMonitor/sheet1";
   let body = {
-    sheet1: {
-      protocol: "sushiswap",
-      extraLabel: "",
-      "date(WithTime)": 10,
-      chainID: "null",
-      address: "0x123",
-    },
+    sheet1: data,
   };
   try {
-    console.log("trying call");
-
     const sheetyRes = await fetch(url, {
       method: "POST",
       body: JSON.stringify(body),
       headers: {
-        "Authorization": "Bearer a test token for now",
+        "Content-Type": "application/json",
+        Authorization: "Bearer YSB0ZXN0IHRva2VuIGZvciBub3c",
       },
     });
-
     const sheetyJson = await sheetyRes.json();
-    console.log('sheetyJson',sheetyJson );
-    
   } catch (error) {
     console.log("error", error);
   }
 }
-callSheety()
 
-// get data and filter it, return only txs with 0x608060 in input data
-// app.get(`/run`, async function (req, res) {
-//   // res.setHeader("Content-Type", "text/html");
-//   // res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-//   // const headers = new Headers();
-//   // contractsData.map((data) => {
-//   //   const network = data.network;
-//   //   //check each address for total new contract deploys
-//   //   //do for sushi
-//   //   console.log('do for sushi');
-//   //   console.log(data.addresses);
-//   //   data.addresses.sushiswap.map(async (data) => {
-//   //     console.log(data);
-//   //     const filteredTxData = await search(data, network);
-//   //     console.log('filteredTxData', filteredTxData);
-//   //     const newSheetBody = filteredTxData.map((data) => {
-//   //       return {
-//   //         Protocol: "sushiswap",
-//   //         "Extra Label": "",
-//   //         "Date (with time)": data.timestamp,
-//   //         ChainID: networkNames[network],
-//   //         Address: data.creates,
-//   //       };
-//   //     });
-//   //     console.log('filtering done');
-//   //     console.log('newSheetBody', newSheetBody);
-//   //     newSheetBody.map(async (data) => {
-//   //       console.log('posting to google doc');
-//   //       const headers = new Headers();
-//   //       headers.append("Authorization", "Bearer a test token for now");
-//   //       let url =
-//   //         "https://api.sheety.co/555630a0193000542b1d8d8703523324/liveTimeNewContractMonitor/sheet1";
-//   //       let body = {
-//   //         sheet1: data,
-//   //       };
-//   //       const sheetyRes = await fetch(url, {
-//   //         method: "POST",
-//   //         body: JSON.stringify(body),
-//   //         headers: headers,
-//   //       });
-//   //       const sheetyJson = await sheetyRes.json();
-//   //     });
-//   //   });
-//   // });
-//   // res.send({
-//   //   contractsCreated: txdata.map((data) => {
-//   //     return data.creates;
-//   //   }),
-//   //   inputData: txdata.map((data) => {
-//   //     return data.data;
-//   //   }),
-//   //   timestamp: txdata.map((data) => {
-//   //     return data.timestamp;
-//   //   }),
-//   // });
-// });
+async function searchAndUpdateSheetForEachAddress() {
+  contractsData.map((data) => {
+    const network = data.network;
+    //check each address for total new contract deploys
 
-// module.exports = app;
-// app.listen(9000, () => console.log("App listening on port 9000!"));
+    const addressData = Object.entries(data.addresses);
+
+    addressData.map((data) => {
+      const protocolName = data[0];
+      
+      data[1].map(async (data) => {
+        const filteredTxData = await search(data, network);
+        const newSheetBody = filteredTxData.map((data) => {
+          return {
+            protocol: protocolName,
+            extraLabel: "",
+            "date (withTime)": getTime(data.timestamp),
+            chainId: networkNames[network].toString(),
+            address: data.creates,
+          };
+        });
+
+        newSheetBody.map(async (data) => {
+          callSheety(data);
+        });
+      });
+    });
+  });
+}
+
+function getTime(timestamp) {
+  const date = new Date(timestamp * 1000);
+
+  //format is h:m dd/mm/yy
+  const minutes = "0" + date.getMinutes();
+  const hours = "0" + date.getHours();
+  return `${hours.substr(-2)}:${minutes.substr(-2)} ${date.getDate()}/${
+    date.getMonth() + 1
+  }/${date.getFullYear().toString().slice(2)}`;
+}
+
+app.get(`/run`, async function (req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+
+  //run search and update
+  searchAndUpdateSheetForEachAddress();
+
+  res.send({
+    message: "task completed",
+  });
+});
+
+module.exports = app;
+app.listen(9000, () => console.log("App listening on port 9000!"));
